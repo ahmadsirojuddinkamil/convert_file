a377188435ce8d6aca3da84285577623eac41f0a
ahmadsirojuddinkamil <ahmadsirojuddinkamil@gmail.com>
2024-04-12 09:25:56 +0700

    v23 ci/cd

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,15 @@
 use App\Http\Controllers\{commentController,  jpgToPdfController, pdfToJpgController, pdfToPngController,  pngToPdfController};
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test-api', function () {
+    $data = [
+        'message' => 'Ini adalah contoh data JSON laravel',
+        'status' => 'OK'
+    ];
+
+    return response()->json($data, 200);
+});
+
 // Route::controller(commentController::class)->group(function () {
 //     Route::post('/create_comment', 'create');
 //     Route::post('/create_comment/{save_uuid_reply_from_click}/reply', 'reply');
