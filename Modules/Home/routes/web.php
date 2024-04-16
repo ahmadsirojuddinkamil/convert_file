e9b16a5db384cf5062db2634f9152d5ca76439ed
ahmadsirojuddinkamil <ahmadsirojuddinkamil@gmail.com>
2024-04-16 14:20:28 +0700

    v9 storage link

--- Modules/Home/routes/web.php
+++ Modules/Home/routes/web.php
@@ -16,5 +16,10 @@ Route::controller(DeleteConvertController::class)->group(function () {
 Route::get('/storage-link/ba14e920-b536-4790-aaba-c44e42589fa3', function () {
     $targetFolder = base_path().'/storage/app/public';
     $linkFolder = $_SERVER['DOCUMENT_ROOT'].'/storage';
-    symlink($targetFolder, $linkFolder);
+
+    if (!file_exists($linkFolder)) {
+        symlink($targetFolder, $linkFolder);
+    }
+
+    return abort(404);
 });
