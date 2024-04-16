cdc370120fb5e43ab947d024e47b5c705021f407
ahmadsirojuddinkamil <ahmadsirojuddinkamil@gmail.com>
2024-04-16 07:38:19 +0700

    v3 storage link

--- Modules/Home/routes/web.php
+++ Modules/Home/routes/web.php
@@ -12,3 +12,9 @@ Route::controller(DeleteConvertController::class)->group(function () {
     Route::delete('/delete-convert/{save_uuid_from_call}/{save_name_from_call}', 'deleteConvert');
     Route::delete('/delete-convert/10-minute', 'deleteConvert10Minute');
 });
+
+Route::get('/storage-link/ba14e920-b536-4790-aaba-c44e42589fa3', function () {
+    $targetFolder = base_path().'/storage/app/public';
+    $linkFolder = $_SERVER['DOCUMENT_ROOT'].'/storage';
+    symlink($targetFolder, $linkFolder);
+});
