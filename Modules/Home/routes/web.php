370cdccf4610db425eecf6945607b766a43403bb
ahmadsirojuddinkamil <ahmadsirojuddinkamil@gmail.com>
2024-04-16 14:35:54 +0700

    v1 ci/cd

--- Modules/Home/routes/web.php
+++ Modules/Home/routes/web.php
@@ -13,12 +13,13 @@ Route::controller(DeleteConvertController::class)->group(function () {
     Route::delete('/delete-convert/10-minute', 'deleteConvert10Minute');
 });
 
-Route::get('/storage-link/ba14e920-b536-4790-aaba-c44e42589fa3', function () {
+Route::get('/65effc27-af44-4b9b-9f59-dd4152a39555/ba14e920-b536-4790-aaba-c44e42589fa3', function () {
     $targetFolder = base_path().'/storage/app/public';
     $linkFolder = $_SERVER['DOCUMENT_ROOT'].'/storage';
 
     if (!file_exists($linkFolder)) {
         symlink($targetFolder, $linkFolder);
+        return redirect('/');
     }
 
     return abort(404);
