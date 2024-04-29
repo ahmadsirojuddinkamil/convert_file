40e4938486e9c3e80e73b523d80ecb451dc065ed
ahmadsirojuddinkamil <ahmadsirojuddinkamil@gmail.com>
2024-04-29 16:07:32 +0700

    v11 cicd testing

--- Modules/Monitoring/routes/api.php
+++ Modules/Monitoring/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Monitoring\App\Http\Controllers\LoggingController;
+use Modules\Monitoring\App\Http\Controllers\{LoggingController, StorageLinkController};
+
+Route::controller(StorageLinkController::class)->group(function () {
+    Route::get('/generate-storage-link/52951bda-a472-47d8-8f83-d2ca858af657', 'generateStorageLink');
+});
 
 Route::controller(LoggingController::class)->group(function () {
     Route::get('/logging/{save_password_from_call}', 'getDataLogging');
--- Modules/Monitoring/routes/web.php
+++ Modules/Monitoring/routes/web.php
@@ -1,8 +1,1 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Monitoring\App\Http\Controllers\StorageLinkController;
-
-Route::controller(StorageLinkController::class)->group(function () {
-    Route::get('/generate-storage-link/52951bda-a472-47d8-8f83-d2ca858af657', 'generateStorageLink');
-});
